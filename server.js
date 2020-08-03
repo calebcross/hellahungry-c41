253e32603936a7a06bdae60ae33a5ca1b5e23a51
Ernie Hsiung <ernie@wyncode.co>
2020-08-03 12:20:37 -0400

    Add examples, create yelp API proxy

--- server.js
+++ server.js
@@ -5,14 +5,25 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
+// Call out Yelp API
+const getYelpAPI = async () => {
+  return axios.get(
+    'https://api.yelp.com/v3/businesses/search?location="2650 NW 5 Ave, Miami, FL 33127"&term="lunch"&radius=300',
+    { headers: { Authorization: `Bearer ${process.env.YELP_API_KEY}` } }
+  );
+};
+
+app.get('/api/yelp', async (request, response) => {
+  try {
+    const resp = await getYelpAPI();
+    response.json(resp.data.businesses);
+  } catch (e) {
+    console.log(e);
+    response.status(500).send({ error: e.message });
+  }
 });
-// END DEMO
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
